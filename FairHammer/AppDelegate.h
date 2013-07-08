00edfcdb9caa22277f501b4e7c8a1baa7bd1347c
barry <barry@rocudo.com>
2013-07-08 15:01:32 +0100

    Initial Commit

---
+++ FairHammer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FairHammer
+//
+//  Created by barry on 08/07/2013.
+//  Copyright (c) 2013 barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FairHammer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FairHammer
+//
+//  Created by barry on 08/07/2013.
+//  Copyright (c) 2013 barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
